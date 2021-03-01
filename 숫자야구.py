dcc9a5364f3e525515cf95fcbfae001baf233cf8
MinGyu Kim <Kmgkm@naver.com>
2021-03-01 15:43:13 +0900

    수정한 내용 : 각 자리 숫자가 다르도록 랜덤 생성

--- 숫자야구.py
+++ 숫자야구.py
@@ -1,16 +1,18 @@
 import random
-rn = random.randrange(100,1000)
-rn = list(str(rn))
-print("세자리 정수가 랜덤으로 생성되었습니다.")
-
-
+while True:
+    rn = random.randrange(100,1000)
+    rn = list(str(rn))
+    print("세자리 정수가 랜덤으로 생성되었습니다.")
+    if rn[0] == rn[1] or rn[0] == rn[2] or rn[1] == rn[2]:
+        continue
+    break
 
 while True:
     strike = 0
     ball = 0
     print("세자리 정수 답을 예측해보세요 : ", end = "")
     ans = list(input())
-    236   222
+
     for i in range(len(ans)):
         if ans[i] in rn and ans[i] == rn[i]:
             strike += 1
